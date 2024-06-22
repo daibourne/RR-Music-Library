db46618aa9c04ac4f1a5ce735d26d60134e98eff
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-06-22 16:34:39 -0700

    Finish suspense activity

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
+import { createResource as fetchData } from './helper'
 
 function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [data, setData] = useState(null)
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
-    if (search) {
+    if (searchTerm) {
+      setData(fetchData(searchTerm))
       const fetchData = async () => {
         document.title = `${search} Music`
         const response = await fetch(API_URL + search)
@@ -26,23 +28,32 @@ function App() {
       }
       fetchData()
     }
-  }, [search])
+  }, [searchTerm])
 
   const handleSearch = (e, term) => {
     e.preventDefault()
     setSearch(term)
   }
 
-  return (
-    <div>
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <Gallery data={data} />
-      <AlbumView />
-      <ArtistView />
-    </div>
-  );
+  const rednerGallery = () => {
+    if(data){
+      return (
+        <div>
+          <SearchBar handleSearch={handleSearch} />
+          {message}
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Gallery data={data} />
+          </Suspense>
+          <Gallery data={data} />
+          <AlbumView />
+          <ArtistView />
+        </div>
+    )}
+  }
 }
+
+
+
 
 export default App;
 
